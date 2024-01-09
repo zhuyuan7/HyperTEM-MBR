1c1c8fb0845ad7727b6b6e253714d9b13f2aea71
JooJoo <68879766+zhuyuan7@users.noreply.github.com>
2024-01-09 15:08:44 +0900

    Create gru_emb.py
    [GRU_EMBEDDING] user, item_id, beh_id

---
+++ gru_emb.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+
+
+df = pd.read_csv("/home/joo/JOOCML/data/Tmall/real_gru_data.csv")  
+print(df)
+
+# df = pd.DataFrame(data)
+user_behavior_seqs = df.groupby('user')['behavior'].apply(list).reset_index()
+user_item_seqs = df.groupby('user')['item'].apply(list).reset_index()
+max_item_index = max(df['item'])
+max_beh_index = max(df['behavior'])
+
+
+
+user_data = {}
+
+# 데이터를 딕셔너리에 저장
+for user_id, group in df.groupby('user'):
+    item_tensor = torch.tensor(group['item'].tolist())
+    behavior_tensor = torch.tensor(group['behavior'].tolist())
+
+    # 딕셔너리에 사용자 아이디를 키로 하여 데이터 저장
+    user_data[user_id] = {'item': item_tensor, 'behavior': behavior_tensor}
+
+
+
+# GRU 모델 정의
+class GRUModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers):
+        super(GRUModel, self).__init__()
+        self.embedding_item = nn.Embedding(input_size['item'], hidden_size)
+        self.embedding_behavior = nn.Embedding(input_size['behavior'], hidden_size)
+        self.gru = nn.GRU(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.gate_layer = torch.nn.Linear(32, 16)
+        self.gate_sigmoid = torch.nn.Sigmoid()
+
+    def gate(self,A_embedding, B_embedding):
+
+        AB_concat = torch.cat((A_embedding,B_embedding),-1)   # torch.Size([44, 16])  
+        context_gate = self.gate_sigmoid(self.gate_layer(AB_concat))  # torch.Size([44, 16])  
+        return torch.add(context_gate * A_embedding, (1.- context_gate) * B_embedding)  
+
+    def forward(self, item, behavior):
+        item_embed = self.embedding_item(item)  # torch.Size([1, 64])  torch.Size([44, 64])
+        behavior_embed = self.embedding_behavior(behavior) # torch.Size([1, 64])   torch.Size([44, 64])
+        x = self.gate(item_embed, behavior_embed)   # torch.Size([44, 64])
+        _, hidden = self.gru(x)  # [4, 16]
+        return hidden#[-1, :, :]
+
+
+user_patterns = []
+# Hyperparameters
+for i in range(user_id ):
+
+    input_size = {'item': max_item_index+1, 'behavior': max_beh_index+1}
+    hidden_size = 16
+    num_layers = 4
+
+
+# 모델 및 손실 함수, 최적화 함수 초기화
+    model = GRUModel(input_size, hidden_size, num_layers)
+    item_id = user_data[i]['item']
+    behavior = user_data[i]['behavior']
+    output = model(torch.tensor(item_id), torch.tensor(behavior))
+    user_patterns.append(output)
+
+
+    
