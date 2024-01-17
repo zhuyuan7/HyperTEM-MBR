269045847e635064bc32bd2a83d9205cd4c21d8a
JooJoo <68879766+zhuyuan7@users.noreply.github.com>
2024-01-17 18:58:04 +0900

    Update Params.py
    final version

--- Params.py
+++ Params.py
@@ -4,8 +4,10 @@ import argparse
 def parse_args():
 	parser = argparse.ArgumentParser(description='Model Params')
 
-	parser.add_argument('--wandb', default="Tmall", type=str, help='wandb run name')  #"roket_recsys"
-	# parser.add_argument('--wandb', default="IJCAI_15", type=str, help='wandb run name')  #"roket_recsys"
+
+	parser.add_argument('--cuda_num', default=3, type=str, help='cuda_num')  
+	parser.add_argument('--wandb', default="Tmall", type=str, help='wandb run name')  #retail_rocket
+	# parser.add_argument('--wandb', default="IJCAI_15", type=str, help='wandb run name')  #retail_rocket
 # 	#for this model
 	parser.add_argument('--hidden_dim', default=16, type=int, help='embedding size')  
 	parser.add_argument('--gnn_layer', default="[16,16,16]", type=str, help='gnn layers: number + dim')  
@@ -16,34 +18,36 @@ def parse_args():
 	parser.add_argument('--sampNum', default=10, type=int, help='batch size for sampling') 
 	parser.add_argument('--hyperNum', default=128, type=int, help='number of hyperedges')
 	parser.add_argument('--gcn_hops', default=2, type=int, help='number of hops in gcn precessing')
+	# parser.add_argument('--gnn_layer', default=2, type=int, help='number of hops in gcn precessing')
 	parser.add_argument('--mult', default=1, type=int, help='')
 	parser.add_argument('--num_layers', default=3, type=int, help='')
 	
 # 	#for train
-	parser.add_argument('--lr', default=0.05, type=float, help='learning rate')
-	parser.add_argument('--opt_base_lr', default=0.01, type=float, help='learning rate')# 0.004
+	parser.add_argument('--lr', default=0.0003, type=float, help='learning rate')
+	parser.add_argument('--opt_base_lr', default=0.002, type=float, help='learning rate')# 0.004
 	parser.add_argument('--opt_max_lr', default=0.05, type=float, help='learning rate')#0.005  =5e-3
-	parser.add_argument('--opt_weight_decay', default=0.001, type=float, help='weight decay regularizer')
-	parser.add_argument('--meta_opt_base_lr', default=0.001, type=float, help='learning rate') # 0.0004  1e-4
+	parser.add_argument('--opt_weight_decay', default=0.0001, type=float, help='weight decay regularizer')
+	parser.add_argument('--meta_opt_base_lr', default=0.0001, type=float, help='learning rate') # 0.0004  1e-4
 	parser.add_argument('--meta_opt_max_lr', default=0.005, type=float, help='learning rate') # 0.005
-	parser.add_argument('--meta_opt_weight_decay', default=0.001, type=float, help='weight decay regularizer')
-	parser.add_argument('--meta_lr', default=0.08, type=float, help='_meta_learning rate')   # 1e-3
-	parser.add_argument('--hyper_lr', default=0.08, type=float, help='hypergraph learning rate') #1e-3
+	parser.add_argument('--meta_opt_weight_decay', default=0.0001, type=float, help='weight decay regularizer')
+	parser.add_argument('--meta_lr', default=0.003, type=float, help='_meta_learning rate')   # 1e-3
+	parser.add_argument('--hyper_lr', default=0.005, type=float, help='hypergraph learning rate') #1e-3
 	parser.add_argument('--batch', default=8192, type=int, help='batch size')   #8192        
 	parser.add_argument('--meta_batch', default=32, type=int, help='batch size') # 32  
-	parser.add_argument('--SSL_batch', default=30, type=int, help='batch size')
+	parser.add_argument('--SSL_batch', default=60, type=int, help='batch size')   #30
 	parser.add_argument('--reg', default=0.001, type=float, help='weight decay regularizer') #1e-3
 	parser.add_argument('--beta', default=0.005, type=float, help='scale of infoNCELoss')
-	parser.add_argument('--epoch', default=125, type=int, help='number of epochs')  #300
+	parser.add_argument('--epoch', default=400, type=int, help='number of epochs')  #300
 	# parser.add_argument('--decay', default=0.96, type=float, help='weight decay rate')  
 	parser.add_argument('--shoot', default=10, type=int, help='K of top k')
 	parser.add_argument('--inner_product_mult', default=1, type=float, help='multiplier for the result')  
-	parser.add_argument('--inner_product_mult_last', default=3, type=float, help='multiplier for the result')    
+	parser.add_argument('--inner_product_mult_last', default=3, type=float, help='multiplier for the result') 
+	parser.add_argument('--keepRate', default=0.5, type=float, help='ratio of edges to keep')   
 	parser.add_argument('--drop_rate', default=0.8, type=float, help='drop_rate')  
 	parser.add_argument('--drop_rate1', default=0.8, type=float, help='drop_rate')  
 	parser.add_argument('--seed', type=int, default=6)  
 	parser.add_argument('--slope', type=float, default=0.1)  
-	parser.add_argument('--patience', type=int, default=20)
+	parser.add_argument('--patience', type=int, default=50)
 	#for save and read
 	parser.add_argument('--path', default='/home/joo/JOOCML/data/', type=str, help='data path')
 	parser.add_argument('--save_path', default='tem', help='file name to save model and training record')
@@ -93,21 +97,3 @@ def parse_args():
 	return parser.parse_args()
 args = parse_args()
 
-# 
-# args.user = 805506#147894
-# args.item = 584050#99037
-# ML10M
-# args.user = 67788
-# args.item = 8704
-# yelp
-# args.user = 19800
-# args.item = 22734
-
-# swap user and item
-# tem = args.user
-# args.user = args.item
-# args.item = tem
-
-# args.decay_step = args.trn_num
-# args.decay_step = args.item
-# args.decay_step = args.trnNum
